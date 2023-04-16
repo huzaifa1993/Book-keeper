456fbf55e37783a69378427daf0c5639300b9352
huzaifa1993 <huothyfayusuf1993@gmail.com>
2023-04-17 00:34:06 +0100

    save data and retrieve it with local storage

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 
 function showModal() {
     modal.classList.add('show-modal');
@@ -31,6 +33,21 @@ function validate(nameValue,urlValue) {
     return true;
 }
 
+function fetchBookmarks() {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        bookmarks = [
+            {
+                name :'jacinto design',
+                url : 'https://jacinto.design',
+            },
+        ];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    console.log(bookmarks);
+}
+
 function storeBookmark (e) {
     e.preventDefault();
     const nameValue = websiteNameEl.value;
@@ -38,10 +55,20 @@ function storeBookmark (e) {
     if (!urlValue.includes('http://','htpts://')) {
         urlValue = `https://${urlValue}`;
     }
-    console.log(nameValue,urlValue);
     if (!validate(nameValue, urlValue)) {
         return false;
     }
+    const bookmark = {
+        name :nameValue,
+        url :urlValue,
+    };
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
 bookmarkForm.addEventListener('submit', storeBookmark);
+
+fetchBookmarks();
